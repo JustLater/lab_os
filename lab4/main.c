40f5b4bb2daad886dd4e1c75eff626ebf37e86f0
JustLater <121096670+JustLater@users.noreply.github.com>
2024-11-01 11:39:33 +0300

    Update main.c

--- lab4/main.c
+++ lab4/main.c
@@ -5,13 +5,29 @@
 #include <unistd.h>
 #include <errno.h>
 
-void print_usage() {
-    printf("Использование: mychmod [options] file\n");
-    printf("Example options:\n");
-    printf("  +x           - добавляет права на выполнение\n");
-    printf("  u-r          - удаляет права на чтение для владельца\n");
-    printf("  g+rw         - добавляет права на чтение и запись для группы\n");
-    printf("  766          - устанавливает конкретные права доступа\n");
+int is_valid_permission(const char *mode) {
+    if (strlen(mode) == 0) return 0;
+
+    int has_symbolic = 0;
+    int has_numeric = 0;
+
+    // Проверка ввода
+    for (int i = 0; mode[i] != '\0'; i++) {
+        if (mode[i] >= '0' && mode[i] <= '7') {
+            has_numeric = 1;
+        } else if (strchr("ugoa+-,rwx", mode[i]) != NULL) {
+            has_symbolic = 1;
+        } else {
+            return 0;
+        }
+    }
+
+    // Проверка на одновременное использование чисел и символов
+    if (has_numeric && has_symbolic) {
+        return 0;
+    }
+
+    return 1;
 }
 
 int change_permissions(const char* path, const char* mode) {
@@ -23,29 +39,51 @@ int change_permissions(const char* path, const char* mode) {
 
     mode_t new_mode = st.st_mode;
 
-    // Обработка символов chmod
-    if (mode[0] == 'u' || mode[0] == 'g' || mode[0] == 'o') {
-        char operation = mode[1];
-        char permission = mode[2];
-        mode_t mask = 0;
+    // Проверка на валидность
+    if (!is_valid_permission(mode)) {
+        fprintf(stderr, "Ошибка: Неизвестная команда или неправильный формат: %s\n", mode);
+        return -1;
+    }
 
-        if (permission == 'r') mask = S_IRUSR | S_IRGRP | S_IROTH;
-        else if (permission == 'w') mask = S_IWUSR | S_IWGRP | S_IWOTH;
-        else if (permission == 'x') mask = S_IXUSR | S_IXGRP | S_IXOTH;
-        else {
-            fprintf(stderr, "Ошибка: Неизвестный символ разрешения: %c\n", permission);
-            return -1;
-        }
-
-        if (operation == '+') new_mode |= mask;
-        else if (operation == '-') new_mode &= ~mask;
-        else {
-            fprintf(stderr, "Ошибка: Неизвестная операция: %c\n", operation);
-            return -1;
-        }
+    // Проверка на числовой режим
+    if (mode[0] >= '0' && mode[0] <= '7') {
+        new_mode = strtol(mode, NULL, 8); // Преобразование в числовой режим
     } else {
-        // Обработка числового режима
-        new_mode = strtol(mode, NULL, 8);
+        // Обработка символов chmod
+        const char *ptr = mode;
+
+        while (*ptr) {
+            char who = *ptr++;
+            char operation = *ptr++;
+
+            // Обработка разрешений
+            while (*ptr && (*ptr == 'r' || *ptr == 'w' || *ptr == 'x')) {
+                mode_t mask = 0;
+
+                switch (who) {
+                    case 'u':
+                        if (*ptr == 'r') mask = S_IRUSR;
+                        else if (*ptr == 'w') mask = S_IWUSR;
+                        else if (*ptr == 'x') mask = S_IXUSR;
+                        break;
+                    case 'g':
+                        if (*ptr == 'r') mask = S_IRGRP;
+                        else if (*ptr == 'w') mask = S_IWGRP;
+                        else if (*ptr == 'x') mask = S_IXGRP;
+                        break;
+                    case 'o':
+                        if (*ptr == 'r') mask = S_IROTH;
+                        else if (*ptr == 'w') mask = S_IWOTH;
+                        else if (*ptr == 'x') mask = S_IXOTH;
+                        break;
+                }
+
+                if (operation == '+') new_mode |= mask;
+                else if (operation == '-') new_mode &= ~mask;
+
+                ptr++;
+            }
+        }
     }
 
     // Установка новых прав
@@ -53,22 +91,18 @@ int change_permissions(const char* path, const char* mode) {
         perror("Ошибка изменения прав");
         return -1;
     }
-
     return 0;
 }
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
-        print_usage();
+        fprintf(stderr, "Использование: mychmod [mode] file\n");
         return EXIT_FAILURE;
     }
-
     const char* mode = argv[1];
     const char* path = argv[2];
-
     if (change_permissions(path, mode) != 0) {
-        return EXIT_FAILURE;
+        return EXIT_FAILURE; // Возвращаем ошибку, если изменение прав не произошло
     }
-
     return EXIT_SUCCESS;
 }
