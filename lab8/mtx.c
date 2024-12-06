4618941d8531a17bf5fe6b041a41e4565ce4ab4b
JustLater <121096670+JustLater@users.noreply.github.com>
2024-12-07 02:00:37 +0300

    Update mtx.c

--- lab8/mtx.c
+++ lab8/mtx.c
@@ -3,65 +3,58 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#define LIMIT 10
+#define LIMIT 12
 
-int array[1000]; // Массив для записи
+int array[200]; // Массив для записи
 int next_write = 0; // Индекс следующей записи
 pthread_mutex_t mutex; // Мьютекс для синхронизации доступа к массиву
-pthread_cond_t cond;   // Условная переменная для уведомления читателей
 
 void* write_thread() {
     for (int i = 0; i < LIMIT; i++) {
         usleep(10000);
         pthread_mutex_lock(&mutex);
+
         array[next_write] = next_write;
-
         printf("Written: %d\n", next_write);
-
         next_write++;
-        usleep(10000);
-        pthread_cond_broadcast(&cond);
+
         pthread_mutex_unlock(&mutex);
     }
     return NULL;
 }
 
 void* read_thread() {
-    usleep(10000);
     for (int i = 0; i < LIMIT; i++) {
-        usleep(10000);
+        usleep(5000);
         pthread_mutex_lock(&mutex);
+
         while (i >= next_write) {
-            pthread_cond_wait(&cond, &mutex);
+            pthread_mutex_unlock(&mutex);
+            usleep(5000);
+            pthread_mutex_lock(&mutex);
         }
 
         printf("Read: array[%d] = %d tid: %lx\n", i, array[i], pthread_self());
-
         pthread_mutex_unlock(&mutex);
-        usleep(10000);
     }
     return NULL;
 }
 
 int main() {
     pthread_mutex_init(&mutex, NULL);
-    pthread_cond_init(&cond, NULL);
     pthread_t writing_array;
     pthread_t reading_threads[LIMIT];
-    pthread_create(&writing_array, NULL, write_thread, NULL);
 
+    pthread_create(&writing_array, NULL, write_thread, NULL);
     for (int i = 0; i < LIMIT; i++) {
         pthread_create(&reading_threads[i], NULL, read_thread, NULL);
     }
 
     pthread_join(writing_array, NULL);
-
     for (int i = 0; i < LIMIT; i++) {
         pthread_join(reading_threads[i], NULL);
     }
 
     pthread_mutex_destroy(&mutex);
-    pthread_cond_destroy(&cond);
-
     return 0;
 }
