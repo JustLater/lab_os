e839d47153e0af3223fb5b80e9b9d3eaf562d69e
JustLater <121096670+JustLater@users.noreply.github.com>
2024-12-17 21:45:17 +0300

    Update sender.c

--- lab7/sender.c
+++ lab7/sender.c
@@ -6,32 +6,62 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <time.h>
+#include <errno.h>
 
-#define SHM_KEY 1235
 #define BUFFER_SIZE 100
 
 int shmid;
-char *shared_memory;
+char *shared_memory = NULL;
+const char *shm_name = "ftok_file";
 
-void cleanup(int signum) {
-    if (shmid >= 0) {
-        shmctl(shmid, IPC_RMID, NULL);
+void handler(int sig) {
+    printf("[SIGNAL HANDLER] Signal %d received\n", sig);
+
+    if (shared_memory != NULL && shmdt(shared_memory) < 0) {
+        int err = errno;
+        fprintf(stderr, "In shmdt %s (%d)\n", strerror(err), err);
+        exit(1);
     }
+
+    if (shmctl(shmid, IPC_RMID, NULL) < 0) {
+        int err = errno;
+        fprintf(stderr, "In shmctl %s (%d)\n", strerror(err), err);
+        exit(1);
+    }
+
+    unlink(shm_name);
     exit(0);
 }
 
-int main() {
-    struct sigaction sa;
-    sa.sa_handler = cleanup;
-    sigaction(SIGINT, &sa, NULL);
-    signal(SIGINT, cleanup);
 
+int main() {
+
+    FILE *file = fopen(shm_name, "w");
+    if (file) {
+        fclose(file);
+    }
+    else {
+        perror("Failed to create the file");
+        exit(1);
+    }
+
+    // Генерация ключа для разделяемой памяти
+    key_t key = ftok(shm_name, 'R');
+    if (key == -1) {
+        perror("ftok");
+        exit(1);
+    }
 
     // Проверка на существование разделяемой памяти
-    shmid = shmget(SHM_KEY, BUFFER_SIZE, IPC_CREAT | IPC_EXCL | 0666);
+    shmid = shmget(key, BUFFER_SIZE, IPC_CREAT | IPC_EXCL | 0666);
     if (shmid < 0) {
         perror("Another sender is already running.");
         exit(1);
+    }
+
+    if (signal(SIGINT, handler) == SIG_ERR || signal(SIGTERM, handler) == SIG_ERR) {
+        perror("Error in signal");
+        return 1;
     }
 
     shared_memory = shmat(shmid, NULL, 0);
@@ -47,7 +77,5 @@ int main() {
         snprintf(shared_memory, BUFFER_SIZE, "Time: %sPID: %d\n", ctime(&current_time), pid);
         sleep(1);
     }
-
     return 0;
 }
-
